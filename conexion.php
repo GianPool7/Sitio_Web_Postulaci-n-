f1c70bc087105e18bcb4202f12c5687e8f206091
Gian Pool Alvino Cántaro <167150925+GianPool7@users.noreply.github.com>
2024-05-24 16:35:24 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,24 +1,6 @@
 <?php
 
-/*
 
-$con=mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'ibpastor_sistemabolsa'
-);
-
-
-*/
-
-
-$con=mysqli_connect(
-    'localhost',
-    'root',
-    '123',
-    'ibpastor_sistemabolsa'
-);
 
 
 ?>
